ffa1c1ceafe40f09caf0d87d9b30b53c938b85a0
Believeharsh <bontinue@gmail.com>
2025-06-24 17:35:25 +0530

    feat : RestaurantPage UU is completed

---
+++ client/src/pages/RestaurantsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react" ; 
+import RestaurantNavbar from "../components/RestaurantPage/RestarurantNavbar";
+
+const RestaurantPage = () => {
+    return (
+        <>
+            <div className="">
+                <RestaurantNavbar/>
+            </div>
+        </>
+    )
+}
+
+export default RestaurantPage ; 
