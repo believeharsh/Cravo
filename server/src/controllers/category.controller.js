7e6788b39865f93f8a8250995fa4acd251ee08c0
Believeharsh <bontinue@gmail.com>
2025-06-28 17:47:46 +0530

    feat : build the 'getAllCategory'controller

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -1,25 +1,23 @@
-import Category from "../models/category.model.js"
+import Category from "../models/category.model.js";
 import { asyncHandler } from "../services/asyncHandler.js";
-import { apiResponse } from "../services/apiResponse.js"
+import { apiResponse } from "../services/apiResponse.js";
 
 const getAllCategories = asyncHandler(async (req, res) => {
-    console.log("fetaching all categories ...... ");
+  console.log("fetaching all categories ...... ");
 
-    const categories = await Category.find({ isVisible: true })
-        .sort({ displayOrder: 1 })
-        .select('name image slug displayOrder')
-        .lean();
-    return res.status(201)
-        .json(
-            new apiResponse(
-                201,
-                { categories: categories },
-                "all categories fetched succussfully"
-            )
-        );
+  const categories = await Category.find({ isVisible: true })
+    .sort({ displayOrder: 1 })
+    .select("name image slug displayOrder")
+    .lean();
+  return res
+    .status(201)
+    .json(
+      new apiResponse(
+        201,
+        { categories: categories },
+        "all categories fetched succussfully"
+      )
+    );
+});
 
-})
-
-export {
-    getAllCategories
-}
+export { getAllCategories };
