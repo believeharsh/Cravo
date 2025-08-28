71f37e8209ac8c6822bce3be5edb4ec816c81885
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-08-28 22:45:43 +0530

    refactor: reseeded the categories by making script more structured for future use

--- server/src/sample-Data/Categories-Data/Categories.js
+++ server/src/sample-Data/Categories-Data/Categories.js
@@ -38,7 +38,7 @@ export const categoriesToUpload = [
     description:
       'Traditional and aromatic dosas, idlis, and curries from Southern India.',
     imagePath:
-      'https://media.istockphoto.com/id/481149282/photo/south-indian-food.jpg?s=612x612&w=0&k=20&c=w43naq0743XDvzCi5FW_ROvzw4_KaCkuam16sfy3hTc=',
+      'https://img.freepik.com/premium-photo/masala-dosa-with-fresh-green-herbs_1114068-63079.jpg',
   },
   {
     name: 'Chinese',
@@ -62,7 +62,7 @@ export const categoriesToUpload = [
     description:
       'Mouth-watering traditional Indian desserts and confectioneries.',
     imagePath:
-      'https://img.freepik.com/premium-photo/indian-sweets-served-silver-wooden-plate-variety-peda-burfi-laddu-decorative-plate-selective-focus_466689-20585.jpg',
+      'https://st4.depositphotos.com/17366968/20308/i/450/depositphotos_203083546-stock-photo-indian-traditional-bengali-sweet-food.jpg',
   },
   {
     name: 'Street-Food',
@@ -70,10 +70,16 @@ export const categoriesToUpload = [
     description:
       'Popular and tangy on-the-go snacks and dishes from the streets of India.',
     imagePath:
-      'https://media.istockphoto.com/id/643781926/photo/plate-of-samosas.jpg?s=612x612&w=0&k=20&c=mLa_tjqfTf9md7F3mxrHZs46kZyzrAv0pawtbeAgGDw=',
+      'https://img.freepik.com/premium-photo/cheese-vada-pav-grated-cheese-wada-pao-popular-bombay-snack-food_466689-69411.jpg',
   },
 
-  // { name: 'Pizza', slug: 'pizza', description: 'Delicious Italian pizzas.', imagePath: path.resolve(__dirname, '../../images/categories/pizza-categories.webp') },
+  {
+    name: 'Pizza',
+    slug: 'pizza',
+    description: 'Delicious Italian pizzas.',
+    imagePath:
+      'https://media.istockphoto.com/id/1216143110/photo/delicious-freshly-made-pizza.jpg?s=612x612&w=0&k=20&c=tLtv6VfpyJX5waOnJyCtoNbOC9hhuhI1EXKObnSc-QI=',
+  },
 
   {
     name: 'Burgers',
@@ -83,9 +89,21 @@ export const categoriesToUpload = [
       'https://media.istockphoto.com/id/2166719820/photo/close-up-of-hamburger-and-french-fries-in-plate-against-white-background.jpg?s=612x612&w=0&k=20&c=EnUwU6W3Y-hHI47h9kpRjujK0WwR_aeIlZOGRqbxIJ4=',
   },
 
-  // { name: 'Indian', slug: 'indian', description: 'Authentic Indian cuisine.', imagePath: path.resolve(__dirname, '../../images/categories/Indian_categories.webp') },
+  {
+    name: 'Indian',
+    slug: 'indian',
+    description: 'Authentic Indian cuisine.',
+    imagePath:
+      'https://media.istockphoto.com/id/1372205267/photo/traditional-indian-food-thali-served-in-plate-top-view.jpg?s=612x612&w=0&k=20&c=4bbfcgQSgdlv3CClnvt064mz1nk_mgiPC6--cj2m4J0=',
+  },
 
-  // { name: 'Italian', slug: 'italian', description: 'Pasta, risotto, and more.', imagePath: path.resolve(__dirname, '../../images/categories/Itilian_categories.jpg') },
+  {
+    name: 'Italian',
+    slug: 'italian',
+    description: 'Pasta, risotto, and more.',
+    imagePath:
+      'https://media.istockphoto.com/id/172397221/photo/spaghetti-puttanesca.jpg?s=612x612&w=0&k=20&c=6FmVJotQON_6BgXt65SSksdNO4B5gFFXvdpaAhzDoJw=',
+  },
 
   {
     name: 'Desserts',
@@ -95,14 +113,20 @@ export const categoriesToUpload = [
       'https://media.istockphoto.com/id/500362775/photo/tiramisu-dessert.jpg?s=612x612&w=0&k=20&c=lWH9PvaN77Oi-aBd5szdOo2hK2RBTbdi_6HY8ExCfjg=',
   },
 
-  // { name: 'Beverages', slug: 'beverages', description: 'Refreshing drinks and shakes.', imagePath: path.resolve(__dirname, '../../images/categories/beverages_categories.webp') },
+  {
+    name: 'Beverages',
+    slug: 'beverages',
+    description: 'Refreshing drinks and shakes.',
+    imagePath:
+      'https://media.istockphoto.com/id/655123574/photo/soft-drinks-splashing.jpg?s=612x612&w=0&k=20&c=iZcaMSEuzxLRy2lpnUw9NhnTcOhYNgxA3poBbjCsSSc=',
+  },
 
   {
     name: 'Breakfast',
     slug: 'breakfast',
     description: 'Start your day right.',
     imagePath:
-      'https://media.istockphoto.com/id/1093244938/photo/aloo-kanda-poha-or-tarri-pohe-with-spicy-chana-masala-curry-selective-focus.jpg?s=612x612&w=0&k=20&c=RdiABI__UiczOAczgVH-DwboySScODdwO3b8Hcbilbo=',
+      'https://img.freepik.com/premium-photo/indian-breakfast-dish-poha-white-background_55610-1377.jpg',
   },
 
   {
@@ -110,7 +134,7 @@ export const categoriesToUpload = [
     slug: 'healthy',
     description: 'Nutritious and light meals.',
     imagePath:
-      'https://media.istockphoto.com/id/1395140920/photo/peach-blueberry-and-arugula-fresh-fruit-salad-with-cheese-and-almond-nuts-top-view.jpg?s=612x612&w=0&k=20&c=IrRb7_0bFeO5uD0tfBoJxTElJHQ372li_ODfPEd7Vdk=',
+      'https://img.freepik.com/premium-photo/vegetarian-food-plate-white-background_227840-694.jpg',
   },
 
   {
@@ -118,16 +142,32 @@ export const categoriesToUpload = [
     slug: 'fast-food',
     description: 'Quick and tasty options.',
     imagePath:
-      'https://media.istockphoto.com/id/174686395/photo/chicken-wings-take-out.jpg?s=612x612&w=0&k=20&c=Oz2CGIcEqL55NgCi3pfs-Fac1h6sG_WP_X1tdX7m7eM=',
+      'https://media.istockphoto.com/id/1072958370/photo/indian-chilli-chicken-dry-served-in-a-plate-over-moody-background-selective-focus.jpg?s=612x612&w=0&k=20&c=18oVkqFScsxijz3dJyMkv01BUSjtrZDT1jnT-inA7TA=',
   },
 
-  // { name: 'Chinese', slug: 'chinese', description: 'Oriental flavors and noodles.', imagePath: path.resolve(__dirname, '../../images/categories/chinese_categories.jpg') },
+  {
+    name: 'Mexican',
+    slug: 'mexican',
+    description: 'Spicy tacos and burritos.',
+    imagePath:
+      'https://media.istockphoto.com/id/459396345/photo/taco.jpg?s=612x612&w=0&k=20&c=_yCtd6OEb2L8xNal4kC1xvm8HoBp8sry6tcBwmxmPHw=',
+  },
 
-  // { name: 'Mexican', slug: 'mexican', description: 'Spicy tacos and burritos.', imagePath: path.resolve(__dirname, '../../images/categories/mexican_categories.jpg') },
+  {
+    name: 'Vegan',
+    slug: 'vegan',
+    description: 'Plant-based goodness.',
+    imagePath:
+      'https://media.istockphoto.com/id/185119929/photo/plate-of-salad.jpg?s=612x612&w=0&k=20&c=fvT24xQIQ5228C7qPV_Cm2MuRx1WWOY6MZ8qW0aKf7M=',
+  },
 
-  // { name: 'Vegan', slug: 'vegan', description: 'Plant-based goodness.', imagePath: path.resolve(__dirname, '../../images/categories/vegan_categories.jpg') },
-
-  // { name: 'Sushi', slug: 'sushi', description: 'Fresh Japanese sushi rolls.', imagePath: path.resolve(__dirname, '../../images/categories/sushi_categories.jpg') },
+  {
+    name: 'Sushi',
+    slug: 'sushi',
+    description: 'Fresh Japanese sushi rolls.',
+    imagePath:
+      'https://media.istockphoto.com/id/1363531410/photo/maki-sushi-philadelphia-on-plate-isolated-on-white.jpg?s=612x612&w=0&k=20&c=Ob3RqwgJ035f9NtISrjdLyiIBIhPeFIifGHHfW3m7YE=',
+  },
 
   {
     name: 'Sandwiches',
@@ -137,11 +177,29 @@ export const categoriesToUpload = [
       'https://media.istockphoto.com/id/137948144/photo/egg-salad-sandwich.jpg?s=612x612&w=0&k=20&c=mmfYYikqmJTPDdlskkLA04uLEuW-wZ0eniOeXJ6XgIs=',
   },
 
-  // { name: 'Salads', slug: 'salads', description: 'Crisp and fresh salads.', imagePath: path.resolve(__dirname, '../../images/categories/salad_categories.jpeg') },
+  {
+    name: 'Salads',
+    slug: 'salads',
+    description: 'Crisp and fresh salads.',
+    imagePath:
+      'https://media.istockphoto.com/id/185119929/photo/plate-of-salad.jpg?s=612x612&w=0&k=20&c=fvT24xQIQ5228C7qPV_Cm2MuRx1WWOY6MZ8qW0aKf7M=',
+  },
 
-  // { name: 'Soups', slug: 'soups', description: 'Comforting and warm soups.', imagePath: path.resolve(__dirname, '../../images/categories/soupe_categories.jpg') },
+  {
+    name: 'Soups',
+    slug: 'soups',
+    description: 'Comforting and warm soups.',
+    imagePath:
+      'https://media.istockphoto.com/id/1350628959/photo/vegetable-soup-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=9JfwZoYSfntD2-fMV6m8q6f_QDVfcwUX_QW58S_YJMA=',
+  },
 
-  // { name: 'BBQ', slug: 'bbq', description: 'Smoky and savory BBQ dishes.', imagePath: path.resolve(__dirname, '../../images/categories/bbq_categories.jpg') },
+  {
+    name: 'BBQ',
+    slug: 'bbq',
+    description: 'Smoky and savory BBQ dishes.',
+    imagePath:
+      'https://www.shutterstock.com/image-photo/top-view-delicious-grilled-beef-600nw-2084951590.jpg',
+  },
 
   {
     name: 'Dinner',
@@ -156,8 +214,6 @@ export const categoriesToUpload = [
     slug: 'lunch',
     description: 'Indian thali perfect for lunch. ',
     imagePath:
-      'https://t4.ftcdn.net/jpg/15/09/38/83/360_F_1509388368_hb9N9ZBrUAPq3O3N2kDSQHpaPW1q3x75.jpg',
+      'https://www.shutterstock.com/image-photo/indian-traditional-special-thali-food-600nw-1477286186.jpg',
   },
-
-  // new Categories
 ];
