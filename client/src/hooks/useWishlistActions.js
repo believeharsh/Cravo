6ee89f2999c0bb57eb3fd61250665fa4baaa6e0c
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-09-16 22:19:22 +0530

    feat: create the new custom hook for wishlistActions

---
+++ client/src/hooks/useWishlistActions.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  fetchAllWishlists,
+  removeItemFromWishlist,
+  addItemToWishlist,
+} from '../features/wishList/wishListSlice';
+
+export const useFavoriteActions = () => {
+  const dispatch = useDispatch();
+
+  const { lists, loading } = useSelector(state => state.wishlist);
+
+  useEffect(() => {
+    if (lists.length === 0 && loading === 'idle') {
+      dispatch(fetchAllWishlists());
+    }
+  }, [dispatch, lists.length, loading]);
+
+  const handleAddItemToWishlist = ({ listId, itemId, itemType }) => {
+    dispatch(addItemToWishlist({ listId, itemId, itemType }));
+  };
+
+  const handleRemoveItemFromWishlist = ({ listId, itemId, itemType }) => {
+    dispatch(removeItemFromWishlist({ listId, itemId, itemType }));
+  };
+
+  // Return the state and action functions for the component to use
+  return {
+    lists,
+    loading,
+    handleAddItemToWishlist,
+    handleRemoveItemFromWishlist,
+  };
+};
