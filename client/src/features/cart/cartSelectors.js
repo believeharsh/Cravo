74c1434380db4e9386093517f710570c4ed91f14
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-09-20 23:22:56 +0530

    feat: updated the cartSelectors to correctly select the state

--- client/src/features/cart/cartSelectors.js
+++ client/src/features/cart/cartSelectors.js
@@ -1,15 +1,23 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-// A simple selector to get the items array from the state.
-const selectCartItems = state => state.cart.items;
+// A simple selector to get the entire cart state from the Redux store.
+export const selectCartState = state => state.cart;
 
-// A memoized selector that calculates the total quantity of all items in the cart.
-export const selectCartTotalQuantity = createSelector(
-  [selectCartItems],
-  items => items.reduce((total, item) => total + item.quantity, 0)
+// A memoized selector that gets the items array from the cart state.
+export const selectCartItems = createSelector(
+  [selectCartState],
+  cart => cart.items
 );
 
-// A memoized selector that calculates the total price of all items in the cart.
-export const selectCartTotalValue = createSelector([selectCartItems], items =>
-  items.reduce((total, item) => total + item.quantity * item.product.price, 0)
+// A memoized selector that gets the total quantity directly from the state,
+// as the backend now provides this value.
+export const selectCartTotalQuantity = createSelector(
+  [selectCartState],
+  cart => cart.totalQuantity
+);
+
+// A memoized selector that gets the total price directly from the state.
+export const selectCartTotalValue = createSelector(
+  [selectCartState],
+  cart => cart.totalPrice
 );
