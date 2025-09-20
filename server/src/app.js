090fa2540fe5c6692fb1752756e84570a58dce1c
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-09-20 23:20:03 +0530

    feat: added the patch Request in the cors

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(
   cors({
     origin: ['http://localhost:5173', 'http://localhost:5174'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     credentials: true,
   })
 );
