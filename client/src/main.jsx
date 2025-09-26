7f581f38d8b8f71c2432cf36ead5ab5fbd6c2f86
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-09-26 12:29:49 +0530

    feat: updated the main.jsx file
    called the setupInterceptors from here
    to mitigate the circular dependency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,23 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import App from './App.jsx';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import store from './app/store';
+// import { Toaster } from 'react-hot-toast';
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <Provider store={store}>
+//       <Toaster position="bottom-center" />
+//       <Router>
+//         <App />
+//       </Router>
+//     </Provider>
+//   </StrictMode>
+// );
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -6,6 +26,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import { Toaster } from 'react-hot-toast';
+
+// import { setupInterceptors } from './api/axiosInstance';
+import { setupInterceptors } from './api/axiosInstance.js';
+import { logout, setAuthState } from './features/auth/authSlice';
+
+setupInterceptors({
+  dispatch: store.dispatch,
+  getState: store.getState,
+  logoutAction: logout,
+  setAuthStateAction: setAuthState,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
