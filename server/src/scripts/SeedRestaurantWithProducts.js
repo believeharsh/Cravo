915ef19687b7d20286498b56f34d64e287c3ce77
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-10-03 11:02:28 +0530

    feat: seeded the new restaurants for the city Mumbai

--- server/src/scripts/SeedRestaurantWithProducts.js
+++ server/src/scripts/SeedRestaurantWithProducts.js
@@ -14,9 +14,7 @@ import Product from '../models/product.model.js';
 import City from '../models/city.model.js';
 
 // Sample Data
-
-import bhopalRestaurants from '../sample-Data/Restaurants-Data/BhopalRestaurants.js';
-// import mumbaiRestaurants from '../sample-Data/Restaurants-Data/MumbaiRestaurants.js';
+import mumbaiRestaurants from '../sample-Data/Restaurants-Data/MumbaiRestaurants.js';
 
 import { productPools } from '../sample-Data/ProductPool/ProductPool.js';
 import { RestaurantImagePool } from '../sample-Data/Restaurant-Image-Pool/RestaurantImagePool.js';
@@ -182,7 +180,7 @@ const seedDatabase = async () => {
     // Ensure indexes on ImageCache
     await ImageCache.init();
 
-    const cityForRestaurants = await City.findOne({ name: 'Bhopal' });
+    const cityForRestaurants = await City.findOne({ name: 'Mumbai' });
     if (!cityForRestaurants) {
       console.warn('city not found. Create city documents first.');
       return;
@@ -195,7 +193,7 @@ const seedDatabase = async () => {
     }, {});
 
     const insertedRestaurants = [];
-    for (const restaurant of bhopalRestaurants) {
+    for (const restaurant of mumbaiRestaurants) {
       const baseLon = restaurant.address.location.coordinates[0];
       const baseLat = restaurant.address.location.coordinates[1];
       const location = {
@@ -239,7 +237,7 @@ const seedDatabase = async () => {
 
       for (const category of restaurantCategories) {
         const pool = productPools[category.name.toLowerCase()] || [];
-        const subset = getRandomSubset(pool, Math.min(3, pool.length));
+        const subset = getRandomSubset(pool, Math.min(4, pool.length));
         for (let i = 0; i < subset.length; i++) {
           const poolProduct = subset[i];
           const productImageUrl = poolProduct.image;
