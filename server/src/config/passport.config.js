6fd5404e44812e66fe91e0ad5456620218744612
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-10-07 08:29:33 +0530

    feat: updated the passport config.js
    used the env varibale instead of the hardcoded url's

--- server/src/config/passport.config.js
+++ server/src/config/passport.config.js
@@ -11,7 +11,7 @@ const configurePassport = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:8000/api/v1/auth/google/callback',
+        callbackURL: `${process.env.API_BASE_URL}/auth/google/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
